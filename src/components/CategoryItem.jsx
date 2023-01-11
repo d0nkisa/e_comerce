f80bd734e4fef962705bfc015c8dd0c75ebdbf94
Donyo Donev <donevdonyo39@gmail.com>
2023-01-11 02:07:04 +0200

    initial commit

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+`;
+
+const CategoryItem = ({item}) => {
+  return (
+    <Container>
+        hello
+    </Container>
+  )
+}
+
+export default CategoryItem;
