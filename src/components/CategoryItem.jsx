5ae3d35265b1ad3ff44e1f1aaf50cf0ecb41e572
Donyo Donev <donevdonyo39@gmail.com>
2023-01-11 23:58:31 +0200

    mapping itens for categories

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -4,12 +4,25 @@ import styled from 'styled-components';
 const Container = styled.div`
     display: flex;
     padding: 20px;
+    justify-content: space-between;
+`;
+const Image = styled.img`
+`;
+const Info = styled.div`
+`;
+const Title = styled.h3`
+`;
+const Button = styled.button`
 `;
 
 const CategoryItem = ({item}) => {
   return (
     <Container>
-        hello
+        <Image src={item.img}/>
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>SHOP NOW</Button>
+        </Info>
     </Container>
   )
 }
